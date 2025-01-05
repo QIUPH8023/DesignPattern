c61443f0d639e464dcd55f1ed4743a22fb5754e4
Cintych930 <155135476+cintychens@users.noreply.github.com>
2025-01-05 19:09:30 +0800

    更新 Other.h用于实现工厂模式

--- Classes/Item/Other.h
+++ Classes/Item/Other.h
@@ -8,30 +8,18 @@
 class Other : public Item
 {
 public:
-	// 构造函数
-	Other(const OtherType _othertype, std::string _image, int _maxstack, int _price);
+	// 静态工厂方法：创建 Other 对象
+    static std::shared_ptr<Other> create(OtherType type, const std::string& image, int maxStack, int price);
 
 	const std::string getDescription() override;
 
 	const OtherType& getSubType();
 
 private:
+    // 构造函数设置为 private
+    Other(const OtherType type, const std::string& image, int maxStack, int price);
 	// 种子
 	OtherType othertype;
-
 };
-
-// 物品类型
-const std::shared_ptr<Other> ITEM_OTHER_FOOD_APPLE = std::make_shared<Other>(FOOD_APPLE, "Items/FOOD_APPLE.png", 200, 1);
-const std::shared_ptr<Other> ITEM_OTHER_FOOD_BREAD = std::make_shared<Other>(FOOD_BREAD, "Items/FOOD_BREAD.png", 200, 1);
-const std::shared_ptr<Other> ITEM_OTHER_FOOD_FRIED_POTATO = std::make_shared<Other>(FOOD_FRIED_POTATO, "Items/FOOD_FRIED_POTATO.png", 200, 1);
-const std::shared_ptr<Other> ITEM_OTHER_FOOD_FISH = std::make_shared<Other>(FOOD_FISH, "Items/FOOD_FISH.png", 200, 1);
-const std::shared_ptr<Other> ITEM_OTHER_CROP_RADISH = std::make_shared<Other>(CROP_RADISH, "Items/CROP_RADISH.png", 200, 1);
-const std::shared_ptr<Other> ITEM_OTHER_CROP_POTATO = std::make_shared<Other>(CROP_POTATO, "Items/CROP_POTATO.png", 200, 1);
-const std::shared_ptr<Other> ITEM_OTHER_CROP_WHEAT = std::make_shared<Other>(CROP_WHEAT, "Items/CROP_WHEAT.png", 200, 1);
-const std::shared_ptr<Other> ITEM_OTHER_MATERIAL_WOOD = std::make_shared<Other>(MATERIAL_WOOD, "Items/MATERIAL_WOOD.png", 200, 1);
-const std::shared_ptr<Other> ITEM_OTHER_MATERIAL_STONE = std::make_shared<Other>(MATERIAL_STONE, "Items/MATERIAL_STONE.png", 200, 1);
-const std::shared_ptr<Other> ITEM_OTHER_MATERIAL_GRASS = std::make_shared<Other>(MATERIAL_GRASS, "Items/MATERIAL_GRASS.png", 200, 1);
-const std::shared_ptr<Other> ITEM_OTHER_PRESENT_FLOWER = std::make_shared<Other>(PRESENT_FLOWER, "Items/PRESENT_FLOWER.png", 200, 1);
 
 #endif // _SEED_H_
